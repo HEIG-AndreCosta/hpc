de0dc832ad56bea187d0ce82cac8986b43bb9f4a
André <andre_miguel_costa@hotmail.com>
2025-02-19 16:38:28 +0100

    feat: complement encode command

--- lab01/main.c
+++ lab01/main.c
@@ -1,28 +1,32 @@
+#include "dtmf.h"
 #include <stdio.h>
 #include <string.h>
 
-void print_usage(const char *prog) {
-    printf("Usage :\n  %s encode input.txt output.wav\n  %s decode input.wav\n", prog, prog);
+void print_usage(const char *prog)
+{
+	printf("Usage :\n  %s encode input.txt output.wav\n  %s decode input.wav\n",
+	       prog, prog);
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
+	if (argc < 3) {
+		print_usage(argv[0]);
+		return 1;
+	}
 
-    if (argc < 3) {
-        print_usage(argv[0]);
-        return 1;
-    }
-    
-    if (strcmp(argv[1], "encode") == 0) {
-        if (argc != 4) {
-            print_usage(argv[0]);
-            return 1;
-        }
-    } else if (strcmp(argv[1], "decode") == 0) {
+	if (strcmp(argv[1], "encode") == 0) {
+		if (argc != 4) {
+			print_usage(argv[0]);
+			return 1;
+		}
+		return dtmf_encode(argv[2], argv[3]) != DTMF_OK;
 
-    } else {
-        print_usage(argv[0]);
-        return 1;
-    }
-    
-    return 0;
+	} else if (strcmp(argv[1], "decode") == 0) {
+	} else {
+		print_usage(argv[0]);
+		return 1;
+	}
+
+	return 0;
 }
