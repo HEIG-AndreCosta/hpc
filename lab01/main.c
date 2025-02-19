508adad36c1d62180531c7a89f5faa106d1c5bec
André <andre_miguel_costa@hotmail.com>
2025-02-19 20:10:36 +0100

    feat(app): read input to encode from file

--- lab01/main.c
+++ lab01/main.c
@@ -1,4 +1,7 @@
 #include "dtmf.h"
+#include "file.h"
+#include "wave.h"
+#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
@@ -20,7 +23,23 @@ int main(int argc, char *argv[])
 			print_usage(argv[0]);
 			return 1;
 		}
-		return dtmf_encode(argv[2], argv[3]) != DTMF_OK;
+		dtmf_encode_t encoder;
+		const char *content = file_read(argv[2]);
+		if (!content) {
+			return EXIT_FAILURE;
+		}
+		int err = (int)dtmf_encode(&encoder, content) != DTMF_OK;
+		free((void *)content);
+
+		if (err != DTMF_OK) {
+			puts(dtmf_err_to_string(err));
+			return EXIT_FAILURE;
+		}
+		err = wave_generate(argv[3], encoder.buffer.data,
+				    encoder.buffer.len, encoder.channels,
+				    encoder.sample_rate);
+		dtmf_terminate(&encoder);
+		return err != 0;
 
 	} else if (strcmp(argv[1], "decode") == 0) {
 	} else {
