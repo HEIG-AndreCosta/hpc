eda1c9bd27b52959986c3c8c63bff4bab151df13
André <andre_miguel_costa@hotmail.com>
2025-02-25 20:55:40 +0100

    feat: measure decoding and encoding time

--- lab01/main.c
+++ lab01/main.c
@@ -3,6 +3,7 @@
 #include "file.h"
 #include "wave.h"
 #include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
 #include <string.h>
 
@@ -29,13 +30,19 @@ int main(int argc, char *argv[])
 		if (!content) {
 			return EXIT_FAILURE;
 		}
+		clock_t t;
+		t = clock();
 		int err = (int)dtmf_encode(&encoder, content) != DTMF_OK;
+		t = clock() - t;
 		free((void *)content);
 
 		if (err != DTMF_OK) {
 			puts(dtmf_err_to_string(err));
 			return EXIT_FAILURE;
 		}
+		const double time_taken = ((double)t) / CLOCKS_PER_SEC;
+		printf("Encoding alone took %g seconds\n", time_taken);
+
 		err = wave_generate(argv[3], encoder.buffer.data,
 				    encoder.buffer.len, encoder.channels,
 				    encoder.sample_rate);
@@ -56,11 +63,16 @@ int main(int argc, char *argv[])
 		decoder.sample_rate = sample_rate;
 		decoder.channels = 1;
 
+		clock_t t;
+		t = clock();
 		char *value = dtmf_decode(&decoder);
+		t = clock() - t;
 		if (!value) {
 			printf("Failed to decode\n");
 			return EXIT_FAILURE;
 		}
+		const double time_taken = ((double)t) / CLOCKS_PER_SEC;
+		printf("Decoding alone took %g seconds\n", time_taken);
 
 		printf("Decoded: %s\n", value);
 
