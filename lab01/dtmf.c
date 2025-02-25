5f6d21588c8aeab8d81ad29840c9d474e2b7f88b
André <andre_miguel_costa@hotmail.com>
2025-02-25 20:36:06 +0100

    fix: imports

--- lab01/dtmf.c
+++ lab01/dtmf.c
@@ -6,8 +6,8 @@
 #include <assert.h>
 #include <math.h>
 #include <ctype.h>
-#include <sndfile-64.h>
 #include <stdint.h>
+#include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
