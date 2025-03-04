991f822872456eeadf99a3153b66fe45857e8967
André <andre_miguel_costa@hotmail.com>
2025-03-04 11:27:34 +0100

    fix: remove decode lookup from decode script

--- lab01/run_decode.sh
+++ lab01/run_decode.sh
@@ -4,7 +4,6 @@
 
 for file in audio/*; do
 	echo "Decoding $file"
-	./build/dtmf_encdec decode_lookup $file
 	./build/dtmf_encdec decode $file
 	echo "----------------------------------------"
 done
