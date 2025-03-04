70758acb269fd73ce67685b80fa4059f1485c383
André <andre_miguel_costa@hotmail.com>
2025-03-04 20:17:49 +0100

    feat(run_decode script): add better indications

--- lab01/run_decode.sh
+++ lab01/run_decode.sh
@@ -3,7 +3,10 @@
 
 
 for file in audio/*; do
-	echo "Decoding $file"
+	echo "Decoding $file" 
+	echo "-- Frequency Domain --"
 	./build/dtmf_encdec decode $file
+	echo "-- Time Domain --"
+	./build/dtmf_encdec decode_time_domain $file
 	echo "----------------------------------------"
 done
