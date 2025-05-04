c4dc50c4d61e1b5d23a1887716537af1fcf9b7c1
André <andre_miguel_costa@hotmail.com>
2025-05-04 10:54:02 +0200

    test: add actual pixel values

--- lab04/code/part1/src/test-simd.c
+++ lab04/code/part1/src/test-simd.c
@@ -10,26 +10,31 @@
 
 int main(void)
 {
-	uint8_t a[16];
-	uint8_t b[16];
-	srand(time(NULL));
+	uint8_t a[16] = { 178, 130, 144, 173, 125, 139, 170, 122,
+			  138, 170, 122, 138, 171, 123, 139, 167 };
+	uint8_t b[16] = { 115, 107, 146, 0, 115, 107, 146, 0,
+			  115, 107, 146, 0, 115, 107, 146, 0 };
 
+#if 0
+	srand(time(NULL));
 	for (size_t i = 0; i < 16; ++i) {
-		a[i] = rand() % (UINT8_MAX + 1);
+		a[i] = 0;
 		b[i] = rand() % (UINT8_MAX + 1);
 	}
+#endif
 
 	uint32_t expected_distances[4] = { 0 };
 	uint32_t actual_distances[4] = { 0 };
 
 	for (size_t i = 0; i < 4; i++) {
 		printf("A %#x %#x %#x\n", a[i * 4], a[i * 4 + 1], a[i * 4 + 2]);
-		printf("B %#x %#x %#x\n", a[i * 4], b[i * 4 + 1], a[i * 4 + 2]);
+		printf("B %#x %#x %#x\n", a[i * 4], b[i * 4 + 1], b[i * 4 + 2]);
 
 		expected_distances[i] =
 			distance_single_pixel(a + i * 4, b + i * 4);
-
-		distance_simd(a + i * 4, b + i * 4, actual_distances + i);
+	}
+	for (size_t i = 0; i < 16; i += 4) {
+		distance_simd(a + i, b + i, actual_distances + i);
 	}
 
 	bool ok = true;
