7ce50e8a2773ea8e12861e5a155e1f63f2b21f16
André <andre_miguel_costa@hotmail.com>
2025-05-04 08:11:21 +0200

    test: compute expected distance using correct function

--- lab04/code/part1/src/test-simd.c
+++ lab04/code/part1/src/test-simd.c
@@ -26,17 +26,8 @@ int main(void)
 		printf("A %#x %#x %#x\n", a[i * 4], a[i * 4 + 1], a[i * 4 + 2]);
 		printf("B %#x %#x %#x\n", a[i * 4], b[i * 4 + 1], a[i * 4 + 2]);
 
-		uint32_t red = a[i * 4] - b[i * 4];
-		uint32_t green = a[i * 4 + 1] - b[i * 4 + 1];
-		uint32_t blue = a[i * 4 + 2] - b[i * 4 + 2];
-		printf("Diff %#x %#x %#x\n", red, green, blue);
-		red = red * red;
-		green = green * green;
-		blue = blue * blue;
-		printf("Squared %#x %#x %#x\n", red, green, blue);
-
-		expected_distances[i] = red + green + blue;
-		printf("Distance %#x\n", expected_distances[i]);
+		expected_distances[i] =
+			distance_single_pixel(a + i * 4, b + i * 4);
 
 		distance_simd(a + i * 4, b + i * 4, actual_distances + i);
 	}
